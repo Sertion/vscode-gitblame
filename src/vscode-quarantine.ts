98baffd98a4fa285a373a4fdd6626c0564b2747a
Albin Jacobsson <albin.jacobsson@gmail.com>
2026-03-24 21:30:15 +0100

    Formating

--- src/vscode-quarantine.ts
+++ src/vscode-quarantine.ts
@@ -3,8 +3,6 @@ import type * as Quarantined from "vscode";
 let vscode: typeof Quarantined | undefined | null = null;
 
 export async function getvscode(): Promise<typeof Quarantined | undefined> {
-	if (vscode === null) {
-		vscode = await import("vscode").catch(() => undefined);
-	}
+	vscode ??= await import("vscode").catch(() => undefined);
 	return vscode;
 }
